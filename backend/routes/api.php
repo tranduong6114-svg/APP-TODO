ed694fa1e0c5595d0ccd7ae910785a89b3f8a0e7
Tran Duong <tranduong6114@gmail.com>
2026-08-16 22:54:14 +0700

    feat(backend): implement categories CRUD API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,5 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('categories', CategoryController::class);
 });
